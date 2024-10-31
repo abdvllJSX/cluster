4c840cd5c84788bce35f1faefd27366d5a0d5040
Abdvlljsx <alimiabdullahi93@gmail.com>
2024-10-31 14:15:51 +0100

    BUG FIXES : NAVBAR

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -18,13 +18,13 @@ const Navbar = () => {
     }, [prevScrollPos]);
 
     return (
-        <nav className={`border-b bg-white fixed inset-x-0 top-0 z-50 py-[2rem] border-b-[#E0E0E0] transition-transform duration-300 ${
+        <nav className={`border-b bg-white fixed inset-x-0 top-0 z-50 py-[2rem] sm:py-[1rem] border-b-[#E0E0E0] transition-transform duration-300 ${
             visible ? 'transform translate-y-0' : 'transform -translate-y-full'
         }`}>
             <MaxContainer>
                 <div className="container flex justify-between items-center">
                     <Link to={'/'}>
-                        <img src="/nav/cluster_logo.svg" className="w-[13rem] h-auto" alt="logo" />
+                        <img src="/nav/cluster_logo.svg" className="w-[13rem] sm:w-[10rem] h-auto" alt="logo" />
                     </Link>
                     <div className="flex items-center gap-[1rem]">
                         <p className="font-[500]">DataSquid</p>
